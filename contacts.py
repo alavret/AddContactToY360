609e790923643a23c936e867faf27ad774943bcd
Andrey Lavretsky <alavret@hotmail.com>
2025-03-28 13:35:41 +0300

    .

--- contacts.py
+++ contacts.py
@@ -24,6 +24,46 @@ def add_contacts_from_file():
     #         data.append(entry)
     # print(data)
 
+
+    data = []
+    print_data = []
+    full_line = ''
+    print('*' * 100)
+    print('Data to import')
+    print('-' * 100)
+    with open(contact_file_name, 'r', encoding='utf-8') as csvfile:
+        for line in csvfile:
+            if line.startswith(','):
+                if len(full_line) > 0:
+                    #print(f'Comma count: {full_line.count(",")}, {full_line.count("CN=")} {full_line}')
+                    entry = ProcessLine(full_line)
+                    if entry:
+                        data.append(entry)
+                        #print_data.append(full_line)
+
+                full_line = line.strip()
+                continue
+            else:
+                if len(full_line) > 0:
+                    full_line += ',' + line.strip()
+    if len(full_line) > 0:
+        #print(f'Comma count: {full_line.count(",")}, {full_line.count("CN=")} {full_line}')
+        entry = ProcessLine(full_line)
+        if entry:
+            data.append(entry)
+            #print_data.append(full_line)
+    
+    # for line in print_data:
+    #     print(line)
+    print('-' * 100)
+    answer = input("Continue to import? (Y/n): ")
+    if answer.upper() in ["Y", "YES"]:
+        organization.post_create_contact(data)
+        return data
+    else:
+        return []
+    
+def ProcessLine(full_line):
     regex = re.compile(
     r"(?i)"  # Case-insensitive matching
     r"(?:[A-Z0-9!#$%&'*+/=?^_`{|}~-]+"  # Unquoted local part
@@ -34,89 +74,61 @@ def add_contacts_from_file():
     r"[A-Z0-9](?:[A-Z0-9-]*[A-Z0-9])?"  # Domain name
     r"\.(?:[A-Z0-9](?:[A-Z0-9-]*[A-Z0-9])?)+"  # Top-level domain and subdomains
     )
-    data = []
-    print_data = []
-    full_line = ''
-    with open(contact_file_name, 'r', encoding='utf-8') as csvfile:
-        for line in csvfile:
-            if line.startswith(','):
-                if len(full_line) > 0:
-                    #print(f'Comma count: {full_line.count(",")}, {full_line.count("CN=")} {full_line}')
-                    entry= {}
-                    print_entry = {}
-                    email_entry = {}
-                    email_list = []
-                    phone_entry = {}
-                    phone_list = []
-                    part1 = ','.join(full_line.split(",")[0:4])
-                    part2 = ','.join(full_line.split(",")[4:])
-                    print_entry['name'] = part1.split(",")[1]
-                    print_entry['surname'] = part1.split(",")[3]
-                    print_entry['email'] = re.findall(regex, part2)
-                    if part1.split(",")[1]:
-                        entry['firstName'] = part1.split(",")[1]
-                    else:
-                        entry['firstName'] = ' '
-                    if part1.split(",")[3]:
-                        entry['lastName'] = part1.split(",")[3]
-                    entry['address'] = ''
-                    entry['company'] = ''
-                    entry['department'] = ''
-                    entry['externalId'] = ''
-                    entry['middleName'] = ''
-                    entry['title'] = ''
-                    email_index = 1
-                    unique_emails = []      
-                    for email in re.findall(regex, part2):
-                        if email not in unique_emails:
-                            email_entry = {}
-                            unique_emails.append(email)
-                            if email_index == 1:
-                                email_entry['main'] = True
-                            else:
-                                email_entry['main'] = False
-                            email_entry['email'] = email                        
-                            email_entry['type'] = 'work'                        
-                            email_list.append(email_entry)
-                            email_index += 1
-                    entry['emails'] = email_list.copy()
-                    phone_entry['phone'] = ''
-                    phone_entry['main'] = True
-                    phone_entry['type'] = 'work'
-                    phone_list.append(phone_entry)
-                    entry['phones'] = phone_list.copy()
-                    if len(email_list) > 0 and (part1.split(",")[1] or part1.split(",")[3]) and full_line.count("CN=") == 1:
-                        print(entry)
-                        data.append(entry)
-                        print_data.append(print_entry)
-                    else:
-                        print(f'Bad or suspicious string: {full_line}')
-
-                full_line = line.strip()
-                continue
-            else:
-                if len(full_line) > 0:
-                    full_line += ',' + line.strip()
-
-
-
-    print('*' * 100)
-    print('Data to import')
-    print('-' * 100)
-    for line in print_data:
-        print(line)
-    print('-' * 100)
-    answer = input("Continue to import? (Y/n): ")
-    if answer.upper() in ["Y", "YES"]:
-        organization.post_create_contact(data)
-        return data
+    entry= {}
+    print_entry = {}
+    email_entry = {}
+    email_list = []
+    phone_entry = {}
+    phone_list = []
+    part1 = ','.join(full_line.split(",")[0:4])
+    part2 = ','.join(full_line.split(",")[4:])
+    print_entry['name'] = part1.split(",")[1]
+    print_entry['surname'] = part1.split(",")[3]
+    print_entry['email'] = re.findall(regex, part2)
+    if part1.split(",")[1]:
+        entry['firstName'] = part1.split(",")[1]
     else:
-        return []
+        entry['firstName'] = ' '
+    if part1.split(",")[3]:
+        entry['lastName'] = part1.split(",")[3]
+    entry['address'] = ''
+    entry['company'] = ''
+    entry['department'] = ''
+    entry['externalId'] = ''
+    entry['middleName'] = ''
+    entry['title'] = ''
+    email_index = 1
+    unique_emails = []      
+    for email in re.findall(regex, part2):
+        if email not in unique_emails:
+            email_entry = {}
+            unique_emails.append(email)
+            if email_index == 1:
+                email_entry['main'] = True
+            else:
+                email_entry['main'] = False
+            email_entry['email'] = email                        
+            email_entry['type'] = 'work'                        
+            email_list.append(email_entry)
+            email_index += 1
+    entry['emails'] = email_list.copy()
+    phone_entry['phone'] = ''
+    phone_entry['main'] = True
+    phone_entry['type'] = 'work'
+    phone_list.append(phone_entry)
+    entry['phones'] = phone_list.copy()
+    if len(email_list) > 0 and (part1.split(",")[1] or part1.split(",")[3]) and full_line.count("CN=") == 1:
+        #print(entry)
+        print(print_entry)
+        return entry
+    else:
+        print(f'Bad or suspicious string: {full_line}')
+        return {}
     
 def get_all_contacts():
     organization.get_all_contacts(file=True)
     print('-' * 100)
-    print('Contacts list was saved to contacts_output.csv file')
+    print('Contacts list was saved to contacts_output.txt file')
     print('-' * 100)
 
 def delete_all_contacts():
